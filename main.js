07a57515338176235616a30353bb7dd5e5f37265
hdtruong <johndoe@example.com>
2017-03-13 15:30:05 -0700

    User can view letters #1.

---
+++ main.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+var text = 'The fox is quick.'
+
+for (var i = 0; i < text.length; i++) {
+  var $letter = document.createElement('span') // Create span element
+  $letter.textContent = text[i]                // Getting letter out of text
+  document.body.appendChild($letter)           // Add span to body of document
+
+  var $firstLetter = document.querySelector('span')
+  $firstLetter.classList.add('current')
+}
