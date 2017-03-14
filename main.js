796ad795d1897191da3e2995c85794fda1446894
hdtruong <johndoe@example.com>
2017-03-14 09:35:48 -0700

    user can see prompt for current letter #2.

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ for (var i = 0; i < text.length; i++) {
   var $letter = document.createElement('span') // Create span element
   $letter.textContent = text[i]                // Getting letter out of text
   document.body.appendChild($letter)           // Add span to body of document
-
-  var $firstLetter = document.querySelector('span')
-  $firstLetter.classList.add('current')
 }
+  var $firstLetter = document.querySelector('span')  //Assign reference to the first span
+  $firstLetter.classList.add('current')              //Change class name to 'current'
